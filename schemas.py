784dd92817f599a9085e4eed2d6a182b60e49f05
Bekmurodo <zsuhrob240@gmail.com>
2025-07-22 18:55:39 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -25,8 +25,12 @@ class Settings(BaseModel):
     authjwt_secret_key: str = '5ffc4280efac0a3b89f3e014d1b3e373eba3bc15b5ea22bc3c6fff60b6de9d89'
 
 class LoginModel(BaseModel):
-    username: str
+    username_or_email: str
     password: str
+
+
+
+
 
 
 
